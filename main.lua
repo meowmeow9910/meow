c5c331e6ba9534894ebfb79548f6461e5ba56821
Liquid <jaredsapian603@gmail.com>
2026-06-01 18:59:20 +0800

    Modify Discord notification and add link copying
    Updated notification title and text for Discord server invitation. Added functionality to copy the link when the notification button is clicked.

--- main.lua
+++ main.lua
@@ -1,15 +1,22 @@
 
 print("jnkie")
 game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "Liquid | Discord Server",
-    Text = "Join to our discord server for more updates!",
-    Icon = "", -- NO ICON CUZ I DON'T WANT
+    Title = "Liquid Hub",
+    Text = "Join our Discord server!",
     Duration = 67,
-    Button1 = "Copy",
-    Callback = function()
-        setclipboard("https://discord.gg/jYkbeWtYsf")
-      print("Copied!")
-    end
+    Button1 = "Copy Link"
 })
 
+game:GetService("StarterGui").NotificationClicked:Connect(function(Text, Button)
+
+    if Button == "Copy Link" then
+
+        if setclipboard then
+            setclipboard("https://discord.gg/jYkbeWtYsf")
+                print("Copied!")
+        end
+
+    end
+
+end)
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/4fb150aae19cf4e71e8033c2b096f606cb82ef5f94982f4e06910779d65633a4/download"))()
