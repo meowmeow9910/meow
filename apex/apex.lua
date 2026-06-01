9d56134dc5dfdaa50f3d2add5c3599a9932958fa
Liquid <jaredsapian603@gmail.com>
2026-06-02 00:20:03 +0800

    Clarify SCRIPT_KEY usage in apex.lua
    Added a comment to clarify the purpose of SCRIPT_KEY.

---
+++ apex/apex.lua
@@ -0,0 +1,3 @@
+getgenv().SCRIPT_KEY = "KEYLESS" -- This should be unchanged because this will automatically enter the key to make it keyless.
+
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/5dc7c0100257cb2d955ebc1b0b6eee04aa340b4c7003199c61f2d1a7ca9aa4cd/download"))()
